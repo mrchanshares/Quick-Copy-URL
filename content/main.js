4a2954279dfa78c0b1fb5d3c8e1081978ea7bab1
mrchanshares <mrchan.shares@gmail.com>
2022-04-24 16:47:33 +0800

    Youtube ShowTitle - Show the video title in the Search box as it changes

--- content/main.js
+++ content/main.js
@@ -1,40 +1,46 @@
-/**
- * Quick Copy URL Browser Extension.
- * Quickly copy the URL of your current tab with a click.
- * 
- * @copyright   Copyright vantezzen (https://github.com/vantezzen)
- * @link        https://github.com/vantezzen/quick-copy-url
- * @license     https://opensource.org/licenses/mit-license.php MIT License
- */
-const copyURL = (sendResponse) => {
-  navigator.clipboard.writeText(window.location.href)
-    .then(() => {
-      sendResponse('success');
-      console.debug('[Quick Copy URL] Text copied to clipboard');
-    })
-    .catch(err => {
-      sendResponse('failed');
-      console.debug('[Quick Copy URL] Could not copy text: ', err);
-    });
-}
 
-chrome.runtime.onMessage.addListener((type, s, sendResponse) => {
-  if (type === "copy") {
-    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-      // Firefox doesn't have clipboard-write permission - directly try to copy
-      copyURL(sendResponse);
-    } else {
-      // Get clipboard write permissions
-      navigator.permissions.query({name: "clipboard-write"}).then(result => {
-        if (result.state == "granted" || result.state == "prompt") {
-          copyURL(sendResponse);
-        } else {
-          sendResponse('failed');
-          // User has not granted access to the clipboard - abort
-          console.debug('[Quick Copy URL] Cannot use clipboard');
+
+// alert('Youtube ShowTitle - Show the video title in the Search box as it changes');
+
+// display youtube title as MV videos changes
+
+// 20220424
+lastTitle = '';
+thisTitle = '';
+thisId = 'search'; //'pageTitle';
+// thisElement = document.getElementById(thisId);
+// allowDuplicate = true;
+// if ((thisElement) &&(!allowDuplicate)) {}
+// else {
+// var btn = document.createElement('BUTTON');
+// btn.id = thisId;
+// btn.classList.add('loop-button');
+// btn.setAttribute('style', 'color: red;');  
+// btn.innerHTML = 'pageTitle here';
+// document.getElementById('logo-icon').appendChild(btn);
+// document.body.appendChild(btn);
+
+// document.body.insertBefore(btn, document.body.firstChild);
+// thisElement = btn;
+
+// }
+
+// document.getElementById( 'search-input').innerText = '123d';
+thisElement = document.getElementById('search-input');
+
+setInterval(function () {
+    // alert(x.length);
+
+    if (thisElement) {
+
+        thisTitle = document.getElementsByTagName('title')[0].innerText;
+        if (thisTitle == lastTitle) {
         }
-      });
+        else {
+
+            thisElement.innerText = thisTitle;
+            lastTitle = thisTitle;
+        }
     }
-    return true;
-  }
-})
+    console.log('quick copy url')
+}, 1000);
